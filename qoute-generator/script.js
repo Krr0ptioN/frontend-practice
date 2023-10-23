fe0cb082cb4e9eaf3d6e281c3a6deb9d2adeb1d8
Krr0ptioN <mardin-pop@protonmail.com>
2023-10-23 20:17:01 +0300

    feat: script to fetch and set the quuote

--- qoute-generator/script.js
+++ qoute-generator/script.js
@@ -1,4 +1,38 @@
 
-// Script init
+let quotes = [];
+const apiURL = 'https://type.fit/api/quotes';
 
-console.log('script-init');
+async function fetchQuotes() {
+    try {
+        const response = await fetch(apiURL);
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        const data = await response.json();
+        quotes = data;
+    } catch (error) {
+        console.error("Error fetching quotes:", error);
+        return [];
+    }
+}
+function randomQuoteId() {
+    const min = 0;
+    const max = quotes.length;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function getRandomQuote() {
+    if (quotes.length == 0) {
+        await fetchQuotes();
+    }
+
+    let quote = quotes[randomQuoteId()];
+    $('#quote').text(quote.text);
+    $('#author').text(quote.author.split(',')[0]);
+
+}
+
+getRandomQuote();
+$('.new-quote-button').click(function() {
+    getRandomQuote();
+});
