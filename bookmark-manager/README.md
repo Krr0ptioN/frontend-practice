fe574bab7505d6e57edf05a703ace37091cdf2a2
Krr0ptioN <mardin-pop@protonmail.com>
2023-10-27 14:40:50 +0300

    feat: closable dialog

--- bookmark-manager/README.md
+++ bookmark-manager/README.md
@@ -7,8 +7,9 @@
 - [X] List bookmarks
 - [ ] Retrieve favicon of the link
 - [ ] Save the bookmarks in local file repopulate the bookmarks in restart
-- [ ] Closable dialog
+- [X] Closable dialog
 - [ ] append bookmark to the body with add dialog
+- [ ] User must be able to remove bookmarks by clicking on times icon
 ### How does it work?
 
 ## Accomplishment
--- bookmark-manager/script.js
+++ bookmark-manager/script.js
@@ -1,4 +1,24 @@
 
-// Script init
+const dialogCloseBtn = $('.dialog-times');
+const dialogContainer = $('.dialog-container');
+const dialogOverlay = $('.dialog-overlay');
+const addBookmarkBtn = $('#bookmark-add-btn');
 
-console.log('script-init');
+function closeDialog() {
+    dialogContainer.hide();
+    dialogOverlay.hide();
+}
+
+function openDialog() {
+    dialogContainer.show();
+    dialogOverlay.show();
+}
+
+
+dialogCloseBtn.click(function() {
+    closeDialog();
+});
+
+addBookmarkBtn.click(function() {
+    openDialog();
+});
