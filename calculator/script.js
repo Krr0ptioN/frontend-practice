09a923f167db6411f7ac872e132341fbb2038e65
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-01 17:43:20 +0300

    feat: Input handling and input update

--- calculator/script.js
+++ calculator/script.js
@@ -3,11 +3,59 @@ const btnPlus = $('#op-plus');
 const btnMinus = $('#op-minus');
 const btnTimes = $('#op-times');
 const btnDivision = $('#op-division');
+const $input = $('.input');
 
-const Number = ;
-
+const numbers = Array.from({ length: 9 }, (_, i) => i + 1);
+const operators = ['+', '-', '/', '*'];
 let expression = "";
 
-array.forEach(element => {
 
+function express(n) {
+    expression += n;
+}
+
+function isOperatorIsLast() {
+    return operators.includes(expression.slice(-1));
+}
+
+function alterOperator(op) {
+    expression = expression.slice(0, -1) + op;
+}
+
+function splitStringByCharacters(inputString, charactersArray) {
+    const regex = new RegExp(`[${charactersArray.join('')}]`, 'g');
+    return inputString.split(regex);
+}
+
+function getLastOperande() {
+    const operandes = splitStringByCharacters(expression, operators);
+    return operandes[operandes.length - 1];
+}
+
+function updateInput() {
+    $input.text(getLastOperande());
+}
+
+numbers.forEach(number => {
+    $(`#num-${number}`).click(() => {
+        express(number);
+        updateInput();
+    });
+});
+
+
+btnPlus.click(() => {
+    isOperatorIsLast() ? alterOperator('+') : express('+');
+});
+
+btnMinus.click(() => {
+    isOperatorIsLast() ? alterOperator('-') : express('-');
+});
+
+btnTimes.click(() => {
+    isOperatorIsLast() ? alterOperator('*') : express('*');
+});
+
+btnDivision.click(() => {
+    isOperatorIsLast() ? alterOperator('/') : express('/');
 });
