b3265dede554e96c07c837aaffe469a19c096c13
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-01 20:23:16 +0300

    feat: evaluation of integers

--- calculator/script.js
+++ calculator/script.js
@@ -23,39 +23,95 @@ function alterOperator(op) {
 }
 
 function splitStringByCharacters(inputString, charactersArray) {
+    console.log(inputString, charactersArray);
     const regex = new RegExp(`[${charactersArray.join('')}]`, 'g');
     return inputString.split(regex);
 }
+
+
+function splitExpression(inputString, charactersArray) {
+    const operatorsPattern = charactersArray.map(char => `\\${char}`).join('|');
+    const regex = new RegExp(`(?<=${operatorsPattern})|(?=${operatorsPattern})`, 'g');
+    return inputString.split(regex).filter(part => part !== '');
+}
+
 
 function getLastOperande() {
     const operandes = splitStringByCharacters(expression, operators);
     return operandes[operandes.length - 1];
 }
 
-function updateInput() {
-    $input.text(getLastOperande());
+function isOperandTwo() {
+    const operandes = splitStringByCharacters(expression, operators);
+    return operandes.length == 2 ? true : false;
+}
+
+function updateInput(output) {
+    $input.text(output);
 }
 
 numbers.forEach(number => {
     $(`#num-${number}`).click(() => {
-        express(number);
-        updateInput();
+        if (isOperatorIsLast()) {
+            express(number);
+            updateInput(number);
+        } else {
+            express(number);
+            updateInput(expression);
+        }
     });
 });
 
+function evaluate(expression) {
+    const parsed = splitExpression(expression, operators);
+    // Second item is always operator
+    switch (parsed[1]) {
+        case '+':
+            return Number(parsed[0]) + Number(parsed[2]);
+        case '-':
+            return Number(parsed[0]) - Number(parsed[2]);
+        case '*':
+            return Number(parsed[0]) * Number(parsed[2]);
+        case '/':
+            return Number(parsed[0]) / Number(parsed[2]);
+    }
+}
+
+function instantEvaulate() {
+    // Global expression
+    expression = String(evaluate(expression));
+    updateInput(expression);
+}
+
+function operatorEventHandler(operator) {
+    if (isOperatorIsLast()) {
+        alterOperator(operator)
+    } else {
+        if (isOperandTwo()) {
+            instantEvaulate();
+            express(operator);
+        } else express(operator);
+    }
+}
 
 btnPlus.click(() => {
-    isOperatorIsLast() ? alterOperator('+') : express('+');
+    operatorEventHandler('+');
 });
 
 btnMinus.click(() => {
-    isOperatorIsLast() ? alterOperator('-') : express('-');
+    operatorEventHandler('-');
 });
 
 btnTimes.click(() => {
-    isOperatorIsLast() ? alterOperator('*') : express('*');
+    operatorEventHandler('*');
 });
 
 btnDivision.click(() => {
-    isOperatorIsLast() ? alterOperator('/') : express('/');
+    operatorEventHandler('/');
+});
+
+$('#op-equal').click(() => {
+    console.log(expression);
+    instantEvaulate();
+    console.log(expression);
 });
