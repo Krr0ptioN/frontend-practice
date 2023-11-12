be3cde3bda0abc1271badb5301f251c8aaa6d602
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-12 10:42:02 +0300

    feat: field validation with color effect

--- form-validator/src/components/Field.tsx
+++ form-validator/src/components/Field.tsx
@@ -5,17 +5,27 @@ interface IFieldProps extends React.PropsWithChildren {
     id: string;
     type: string;
     onFieldChange: (id: string, value: string) => void;
+    inputValidation?: (value: string) => boolean | undefined;
 }
 
-export const Field: React.FC<IFieldProps> = ({ type, id, placeholder, children, onFieldChange }) => {
+export const Field: React.FC<IFieldProps> = ({ type, id, placeholder, children, onFieldChange, inputValidation }) => {
     const [value, setValue] = useState('');
+    const [borderColor, setBorderColor] = useState('ring-gray-400 focus:ring-gray-700');
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = e.target.value;
+
         setValue(newValue);
-        onFieldChange(id, newValue)
+        onFieldChange(id, newValue);
+        if (inputValidation !== undefined) {
+            if (inputValidation(newValue)) {
+                setBorderColor('ring-green-400');
+            } else {
+                setBorderColor('ring-red-400');
+            }
+        }
     }
     return (<div className='w-full'>
-        <label htmlFor={id} className='w-full text-lg' >{children} </label>
-        <input id={id} value={value} onChange={handleInputChange} type={type} className='p-4 my-2 w-full h-10 border-black' placeholder={placeholder} />
+        <label htmlFor={id} className='w-full text-lg font-semibold' >{children} </label>
+        <input id={id} value={value} onChange={handleInputChange} type={type} className={`rounded-md p-4 my-2 w-full ring-2 h-10 ${borderColor}`} placeholder={placeholder} />
     </div >);
 }
--- form-validator/src/components/Form.tsx
+++ form-validator/src/components/Form.tsx
@@ -17,14 +17,37 @@ export const Form: React.FC<IForm> = ({ children }) => {
         else
             console.log('Invalid password');
     };
+
+    const passwordValidation = (value: string) => {
+        return /^.{8,}$/.test(value);
+    }
+    const siteValidation = (value: string) => {
+        const isValidURL = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
+        return isValidURL.test(value);
+    }
+
+    const fullNameValidation = (value: string) => {
+        return /^[A-Za-z\s\.'-]*$/.test(value);
+    }
+    const telValidation = (value: string) => {
+        return /^\+?[0-9\s\-]+$/.test(value);
+    }
+    const emailValidation = (value: string) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+    }
+
+    const confirmedPasswordValidation = (value: string) => {
+        return formData['password'] === value;
+    }
+
     return (
         <form onSubmit={handleSubmit}>
-            <Field onFieldChange={handleFieldValueChange} placeholder='Full Name' id='full-name' type='text'>Full Name</Field>
-            <Field onFieldChange={handleFieldValueChange} placeholder='555-555-5555' id='phone-number' type='tel'>Phone Number</Field>
-            <Field onFieldChange={handleFieldValueChange} placeholder='user@email.com' id='email-address' type='email'>Email Address</Field>
-            <Field onFieldChange={handleFieldValueChange} placeholder='https://site.com' id='website-url' type='url'>Website URL</Field>
-            <Field onFieldChange={handleFieldValueChange} placeholder='Create password (Min. 8 character)' id='password' type='password'>Password</Field>
-            <Field onFieldChange={handleFieldValueChange} placeholder='Confirm Password' id='confirm-password' type='password'>Confirm Password</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={fullNameValidation} placeholder='Full Name' id='full-name' type='text'>Full Name</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={telValidation} placeholder='555-555-5555' id='phone-number' type='tel'>Phone Number</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={emailValidation} placeholder='user@email.com' id='email-address' type='email'>Email Address</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={siteValidation} placeholder='https://site.com' id='website-url' type='url'>Website URL</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={passwordValidation} placeholder='Create password (Min. 8 character)' id='password' type='password'>Password</Field>
+            <Field onFieldChange={handleFieldValueChange} inputValidation={confirmedPasswordValidation} placeholder='Confirm Password' id='confirm-password' type='password'>Confirm Password</Field>
             <button type="submit">Submit</button>
         </form>
     )
