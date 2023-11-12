5d636bd01f5baaff95ae7d8626beffff2c1502ed
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-12 12:40:53 +0300

    feat: message box alert and success

--- form-validator/src/components/Form.tsx
+++ form-validator/src/components/Form.tsx
@@ -4,31 +4,82 @@ import {
     fullNameValidation, passwordValidation,
     siteValidation
 } from "../utils/validation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 interface IForm extends React.PropsWithChildren {
 
 }
 
 export const Form: React.FC<IForm> = ({ children }) => {
     const [formData, setFormData] = useState<{ [key: string]: string }>({});
-    const [validated, setValidated] = useState<boolean>(false);
+    const [msgStatusStyle, setMsgStatusStyle] = useState('border-gray-400 bg-white text-black');
+    const [msgStatus, setMsgStatus] = useState('Don\'t rush :)');
     const handleFieldValueChange = (id: string, value: string) => {
         setFormData((prevFormData: any) => ({ ...prevFormData, [id]: value }));
     }
+    useEffect(() => { }, [msgStatus, msgStatusStyle]);
+    const isValidated = () => {
+        let isAllFieldsValid = true;
 
+        Object.entries(formData).forEach(([fieldName, fieldValue]) => {
+            switch (fieldName) {
+                case 'password':
+                    if (!passwordValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                case 'confirm-password':
+                    if (!confirmedPasswordValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                case 'full-name':
+                    if (!fullNameValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                case 'phone':
+                    if (!telValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                case 'email':
+                    if (!emailValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                case 'website-url':
+                    if (!siteValidation(fieldValue)) {
+                        isAllFieldsValid = false;
+                        break;
+                    }
+                    break;
+                default:
+                // Handle other fields' validation if needed
+            }
+        });
+        return isAllFieldsValid;
+
+    };
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        if (formData['password'] === formData['confirm-password'])
+        if (isValidated()) {
             console.log("Form submitted with data:", formData);
-        else
-            console.log('Invalid password');
+            setMsgStatusStyle('border-green-400 bg-white text-green-400');
+            setMsgStatus('All fields are valid!');
+        } else {
+            setMsgStatusStyle('border-red-500 bg-white text-red-500');
+            setMsgStatus('Invalid input in one or more fields.');
+        }
     };
 
     const confirmedPasswordValidation = (value: string) => {
         return formData['password'] === value;
     }
-
-
 
     return (
         <form onSubmit={handleSubmit}>
@@ -39,6 +90,7 @@ export const Form: React.FC<IForm> = ({ children }) => {
             <Field onFieldChange={handleFieldValueChange} inputValidation={passwordValidation} placeholder='Create password (Min. 8 character)' id='password' type='password'>Password</Field>
             <Field onFieldChange={handleFieldValueChange} inputValidation={confirmedPasswordValidation} placeholder='Confirm Password' id='confirm-password' type='password'>Confirm Password</Field>
             <button className="p-3 my-3 w-full text-2xl font-bold text-white bg-black rounded-md h-17" type="submit">Register</button>
-        </form>
+            <div className={`flex justify-center items-center p-3 border-4 rounded-md text-lg ${msgStatusStyle}`}>{msgStatus}</div>
+        </form >
     )
 };
